946d91d27c85a99a85bef673d3e89ec00843ade6
Rishabh Raj <73957359+rishabhr1@users.noreply.github.com>
2023-12-02 17:48:02 +0530

    Update login.php

--- php/login.php
+++ php/login.php
@@ -22,37 +22,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 
 
-// require_once 'config.php';
-
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     $email = $_POST["userName"];
-//     $password = $_POST["password"];
-
-//     $getExistingUserQuery = "SELECT * FROM user WHERE email='$email' AND password='$password'";
-//     $getExistingUser = $mysql_conn->query($getExistingUserQuery);
-//     foreach ($user as $getExistingUser) {
-//     echo $user . "\n";
-// }
-//     if ($getExistingUser->num_rows == 0) {
-//         echo json_encode(['success' => false, 'data' => "User doesn't exist"]);
-//     } else {
-//         echo json_encode(['success' => true, 'data' => "Login Successful",'user'=>$getExistingUser]);
-//     }
-// }
-
-
-
-    // if ($result->num_rows > 0) {
-    //     // Create a session ID
-    //     $session_id = bin2hex(random_bytes(32));
-
-    //     // Store session ID in Redis with user information
-    //     $redis_conn->set($session_id, json_encode(['username' => $username]));
-
-    //     // Return session ID to the client
-    //     echo json_encode(['session_id' => $session_id]);
-    // } else {
-    //     echo "Invalid username or password";
-    // }
-
-    // $stmt->close();
