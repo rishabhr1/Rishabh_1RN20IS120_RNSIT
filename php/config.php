1ee72db79ea2542438a9d8c11521e96f306c90fa
Rishabh Raj <73957359+rishabhr1@users.noreply.github.com>
2023-12-02 17:47:08 +0530

    Update config.php

--- php/config.php
+++ php/config.php
@@ -8,7 +8,7 @@ if ($mysql_conn->connect_error) {
 
 
 // Mongo Instance
-$mongoManager = new MongoDB\Driver\Manager("mongodb://asimalam8:Asim123@ac-yogi2tv-shard-00-00.liakaku.mongodb.net:27017/fastfoodmern?ssl=true&replicaSet=atlas-lbmxxw-shard-0&authSource=admin&retryWrites=true&w=majority");
+$mongoManager = new MongoDB\Driver\Manager("mongodb://rishabh:rishabh@ac-jzelyqr-shard-00-00.dcqvyle.mongodb.net:27017,ac-jzelyqr-shard-00-01.dcqvyle.mongodb.net:27017,ac-jzelyqr-shard-00-02.dcqvyle.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-od5ub7-shard-0&authSource=admin&retryWrites=true&w=majority ");
 $mongoDatabase = "guvi";
 $mongoCollection = "user";
 
